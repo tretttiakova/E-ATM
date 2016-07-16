6a823fde4cef5bc3bd9bda278ed4f7a6ef64907e
tretttiakova <tretyakova.zoe@yandex.ru>
2016-07-16 16:59:42 +0300

    1

---
+++ User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_ATM
+{
+    class User
+    {
+        public int currentBalance;
+        public string userName, password;
+
+        public void retrieve(int sum)
+        {
+            if (retrievability(sum))
+            {
+                currentBalance -= sum;
+                return;
+            }
+            MessageBox.Show("No enough money", "ERROR");
+        }
+
+        public void remmitance(int sum, string payee) //?
+        {
+            if (!retrievability(sum))
+            MessageBox.Show("No enough money", "ERROR");
+
+        }
+
+        public void deposit(int sum)
+        {
+            currentBalance += sum;
+        }
+
+        bool retrievability(int sum)
+        {
+            if (sum <= currentBalance)
+                return true;
+            return false;
+        }
+
+    }
+}
